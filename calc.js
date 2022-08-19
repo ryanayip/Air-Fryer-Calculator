440689187b0329c0c63a72623dfe492396b477b6
Ryan Yip <ryip94@yahoo.com>
2022-08-19 10:08:32 -0700

    Completed js portion to convert temperature

--- calc.js
+++ calc.js
@@ -1,4 +1,15 @@
-const ovTime = document.querySelector("#ovenTime");
+const ovenTemp = document.querySelector("#ovenTemp");
+const airTemp = document.querySelector("#airTemp");
+const ovenTime = document.querySelector("#ovenTime");
+const airTime = document.querySelector("#airTime");
+const convertBtn = document.querySelector("#convertBtn");
 
-let text = ovTime.innerText
+
+convertBtn.addEventListener("click", (event) => {
+    const newTemp = (ovenTemp.value - "25");
+    const newTime = (ovenTime.value * "0.80");
+
+    airTemp.innerText = `${newTemp} °F`;
+    airTime.innerText = `${newTime} min`;
+})
 
